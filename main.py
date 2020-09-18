e3e3e9818e586fc6639066556297f057df2e5089
goranaviani <goranaviani@gmail.com>
2020-09-18 16:15:36 +0200

    first app

--- main.py
+++ main.py
@@ -20,6 +20,11 @@ def main():
     es.indices.refresh(index="test-index")
     print(res['_source'])
 
+    res = es.search(index="test-index", body={"query": {"match_all": {}}})
+    print("Got %d Hits:" % res['hits']['total']['value'])
+    for hit in res['hits']['hits']:
+        print("%(timestamp)s %(company_name)s: %(Address)s" % hit["_source"])
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
